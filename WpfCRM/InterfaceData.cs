3069ebe8bfef424489d98fe1ff4caed2ed7fe847
Fedor Guru <GURU010@outlook.com>
2021-07-16 01:39:11 +0300

    intarface

---
+++ WpfCRM/InterfaceData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfCRM
+{
+    interface InterfaceData
+    {
+        List<Users> GetUsers();
+    }
+    interface IDataProcessor
+    {
+        void InterfaceData(InterfaceData interfaceData);
+    }
+}
